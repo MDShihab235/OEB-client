7c709847a75a89f738e0472f0b25a8a98d9e81cd
MDShihab235 <msub111244@gmail.com>
2022-08-05 19:45:44 +0600

    after responsive

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+// // Navbar sticky
+// let navbar = document.getElementById("navbar");
+// let sticky = navbar.offsetTop;
+
+// function navbarSticky() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+// window.onscroll = function () {
+//   navbarSticky();
+// };
+
+// Header Images sliders
 let slideIndex = 0;
 showSlides();
 
